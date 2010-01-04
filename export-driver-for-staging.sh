5c3b9223733429756fd4b7904332e9efff651e9a
Jarod Wilson <jarod@redhat.com>
2010-01-04 14:16:34 -0500

    driver: add basic export-to-kernel-git-tree script
    Very little error-checking, but hey, it works so far...
    
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

---
+++ export-driver-for-staging.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# Crude script to export kernel driver bits to a kernel git tree,
+# under drivers/staging/crystalhd/
+# You still need to edit drivers/staging/{Kconfig,Makefile} by hand
+#
+
+if [ -z "$1" ]; then
+  echo "You need to specify a path to a kernel tree"
+  exit 1
+fi
+
+kernelsrc=$1
+dest=$kernelsrc/drivers/staging/crystalhd
+me=$PWD
+
+if [ ! -e $dest ]; then
+  mkdir $dest
+fi
+
+# Copy  files into place in kernel git tree
+cp -a $me/driver/linux/*.c $dest/
+cp -a $me/driver/linux/*.h $dest/
+cp -a $me/include/*.h $dest/
+cp -a $me/include/link/*.h $dest/
+
+# Now run unifdef over the source to strip out legacy compat
+pushd $dest
+perl -pi -e 's|KERNEL_VERSION.*|1|g' *.c *.h
+for f in *.c *.h
+do
+  cp $f tmp-$f
+  unifdef -DLINUX_VERSION_CODE=2 tmp-$f > $f
+  rm -f tmp-$f
+done
+
+# Now show diff and diffstat
+git diff -p --stat
