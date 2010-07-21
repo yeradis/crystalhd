84306614fe62eae5f58c382a12f0a1cc6a25c664
Narendra Sankar <nsankar@broadcom.com>
2010-07-20 17:35:17 -0700

    added Firmware binary license text
    Signed-off-by: Jarod Wilson <jarod@redhat.com>

--- linux_lib/libcrystalhd/libcrystalhd_fwcmds.cpp
+++ linux_lib/libcrystalhd/libcrystalhd_fwcmds.cpp
@@ -25,6 +25,24 @@
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  *
  *******************************************************************/
+
+/*******************************************************************
+
+FIRMWARE BINARIES ARE DISTRIBUTED UNDER THE FOLLOWING LICENSE -
+
+BINARIES COVERED WITH THIS LICENSE ARE bcm70015fw.bin and bcm70012fw.bin
+
+Copyright 2007-2010 Broadcom Corporation
+
+Redistribution and use in binary forms of this software, without modification, are permitted provided that the following conditions are met:
+
+Redistributions must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+Neither the name of Broadcom nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED “AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BROADCOM BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*********************************************************************/
+
 #include "7411d.h"
 #include "libcrystalhd_fwcmds.h"
 #include "libcrystalhd_priv.h"
